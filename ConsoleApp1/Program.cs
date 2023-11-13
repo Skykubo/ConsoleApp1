ba8c0ea78956dbb4c8cb99c739b3e630858ea424
ISPO0122072 Мешавкин Иван Дмитриевич <ISPO0122072@sibupk.su>
2023-11-13 13:36:34 +0700

    сделано 9 задание begin

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -7,18 +7,26 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
 
-            Console.WriteLine("Введите сторону квадрата a: ");
+            //Console.WriteLine("Введите сторону квадрата a: ");
+            //string tmp = Console.ReadLine();
+            //int a = Int32.Parse(tmp);
+
+            //int p = 4 * a;
+            // Console.WriteLine("Периметр квадрата =" + p);
+
+
+            //int s = a * a;
+            //    Console.WriteLine("Площадь квадрата =" + s);
+
+            //Console.WriteLine("hello, world!");
+
+            Console.WriteLine("Введите два неотрицательных числа: ");
             string tmp = Console.ReadLine();
             int a = Int32.Parse(tmp);
-
-            int p = 4 * a;
-             Console.WriteLine("Периметр квадрата =" + p);
-
-
-            int s = a * a;
-                Console.WriteLine("Площадь квадрата =" + s);
-
-            Console.WriteLine("hello, world!");
+            tmp = Console.ReadLine();
+            int b = Int32.Parse(tmp);
+            double result = Math.Sqrt(a * b);
+                Console.WriteLine("Среднее геометрическое чисел = " + result);
 
         }
     }
