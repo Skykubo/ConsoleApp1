a9dfe3711993d90bed47ddacdfadd3e4d96d61e2
ispo0122072 <ispo0122072@CL326-09>
2023-11-13 13:19:23 +0700

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Введите сторону квадрата a: ");
+            string tmp = Console.ReadLine();
+            int a = Int32.Parse(tmp);
+
+            int p = 4 * a;
+             Console.WriteLine("Периметр квадрата =" + p);
+
+
+            int s = a * a;
+                Console.WriteLine("Площадь квадрата =" + s);
+
+
+
+        }
+    }
+}
